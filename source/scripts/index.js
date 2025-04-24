8e9cc4ca0d748641e423929cf8c9810b25c72a97
dennyhappysomuch <den_happy@bk.ru>
2025-04-24 16:05:07 +0700

    Проект завершен

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,43 @@
 /* в этот файл добавляет скрипты*/
+
+const openMenuButton = document.querySelector('.menu-button');
+const mainNav = document.querySelector('.main-nav');
+
+const initMenu = () => {
+  if (openMenuButton) {
+    openMenuButton.addEventListener('click', () => {
+      if (!openMenuButton.classList.contains('menu-button--opened')) {
+        openMenuButton.classList.add('menu-button--opened');
+        mainNav.classList.add('main-nav--opened');
+      } else {
+        openMenuButton.classList.remove('menu-button--opened');
+        mainNav.classList.remove('main-nav--opened');
+      }
+    });
+  }
+};
+
+const resetOpenedStateMenu = () => {
+  const viewport = window.innerWidth;
+
+  if (viewport > 979) {
+    if (openMenuButton.classList.contains('menu-button--opened')) {
+      openMenuButton.classList.remove('menu-button--opened');
+    }
+
+    if (mainNav.classList.contains('main-nav--opened')) {
+      mainNav.classList.remove('main-nav--opened');
+    }
+  }
+};
+
+window.addEventListener('resize', resetOpenedStateMenu);
+
+document.addEventListener('DOMContentLoaded', () => {
+  /* 1. Удалить NO-JS */
+  if (mainNav.classList.contains('main-nav--no-js')) {
+    mainNav.classList.remove('main-nav--no-js');
+  }
+
+  initMenu();
+});
